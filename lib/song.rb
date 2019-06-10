38292e37ef1c7912019a30bc607b52e8b5b5fa2c
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-10 20:13:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,9 +4,15 @@ class Song
 
   def initialize(name)
     @name = name
-
-
   end
+
+  def artist_name
+    if @artist == nil
+      return nil
+    else
+      @artist.name
+      end
+    end
 
 
 
