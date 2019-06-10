e16e4397692532ea7d0c7800adfc10bfed6aff65
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-10 20:08:59 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,7 @@
 class Author
 
 @@post_count = 0
-attr_accessor :name
+attr_accessor :name, :posts
 
   def initialize(name)
     @name = name
@@ -10,6 +10,8 @@ attr_accessor :name
 
   def add_post(post)
     @posts << post
+    post.author = self
+    @@post_count += 1
   end
 
   def self.name
