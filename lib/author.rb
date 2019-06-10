e82a3b8ac616e95928e2eb451f57bb9c86989023
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-10 20:03:13 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,22 +2,39 @@ class Author
 
 attr_accessor :name
 
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
-def iniitalize(name)
-  @name = name
-end
+  def add_song(song)
+    @songs << song
+  end
 
-def add_post(post)
+  def self.name
+    @name
+  end
+
+  def songs
+  @songs
+  end
+
+  def add_song(song)
+    @@song_count += 1
+    @songs << song
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    s = Song.new(name)
+    self.add_song(s)
+    s
+
+  end
+
+  def self.song_count
+    @@song_count
+  end
 
 
-end
-
-def posts
-  @posts
-end
-
-def name
-  @name
-end
-
-end
+  end
