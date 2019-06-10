a205f124242189d9d3ad331de118721f8e22dd70
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-10 20:23:40 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,23 +1,23 @@
 class Post
-
+  
   @@all = []
-
-attr_accessor :title, :author
-
-def iniitalize(title)
-  @title = title
-end
-
-def author_name
-  if @author == nil
-    return nil
-  else
-    @author.name
+  
+  attr_accessor :title, :author
+  
+  def iniitalize(title)
+    @title = title
+  end
+  
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
     end
   end
-
+  
   def self.all
     @@all
   end
-
+  
 end
